4cd34fe756968f974e2052ccbd4f2684108305ea
studentTry6e7e8 <240428754@stu.vtc.edu.hk>
2026-03-30 06:23:49 +0000

    Complete assignment

--- GenNum.java
+++ GenNum.java
@@ -4,7 +4,7 @@ public class GenNum {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= i+1; j++) {
                 System.out.print(j);
             }
             System.out.println();

75e36fb47d150552931b1a8fa01584dbd22081ad
studentTry6e7e8 <240428754@stu.vtc.edu.hk>
2026-03-30 06:35:50 +0000

    Complete assignment

--- GenNum.java
+++ GenNum.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class GenNum {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 7;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i+1; j++) {
                 System.out.print(j);
+                if (j==7)
+                    break;
             }
             System.out.println();
         }
